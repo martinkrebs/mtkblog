07932244b214a02f1c6252e27f467b6958a7a295
Martin Krebs <martin.t.krebs@gmail.com>
2017-03-26 16:54:16 +0100

    hello world index route view and template

---
+++ mtkblog/settings/production.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from .base import *
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    'www.martintkrebs.com',
+    'martintkrebs.com',
+
+    'localhost',  # temp, just for local dev
+]
