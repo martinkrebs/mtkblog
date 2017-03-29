6822c7ae2b6896f813dbeed2e2286fc68c0c20a7
Martin Krebs <martin.t.krebs@gmail.com>
2017-03-29 15:54:50 +0100

    Added custom admin class to customize admin display of Post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('author', 'status', 'created', 'publish')
+    orderiing = 'publish'
+    search_fields = ('title', 'body')
+    date_hierarchy = 'publish'
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Post, PostAdmin)
