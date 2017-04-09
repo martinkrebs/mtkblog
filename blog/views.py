177ffc90fa26238699f6a42f2eadf22daf3f0a17
Martin Krebs <martin.t.krebs@gmail.com>
2017-04-09 17:07:39 +0100

    Added List and Detail views/templates. Added links from List item to Detail view. Added links from Detail view back to list view.

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
-def index(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/index.html', {'posts': posts})
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def detail(request, id):
-    post = get_object_or_404(Post, pk=id)
-    return render(request, 'blog/detail.html', {'post': post})
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        slug=post,
+        status='published',
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
+    )
+    return render(request, 'blog/post/detail.html', {'post': post})
